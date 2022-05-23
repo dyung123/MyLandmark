d73407869530d24738b36c82fce9b7b23f570846
robot <kimdydydy@naver.com>
2022-05-23 13:59:34 +0900

    이미지 클릭 이벤트 처리

--- app/src/main/java/com/example/mylandmark/infoActivity.java
+++ app/src/main/java/com/example/mylandmark/infoActivity.java
@@ -3,13 +3,16 @@ package com.example.mylandmark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class infoActivity extends AppCompatActivity {
+public class infoActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView img_info;
-    TextView txt_info;
+    TextView  txt_info;
+    Button    btn_info_back;
     int imgno = 0;
     int imgs[] = {R.drawable.info1 , R.drawable.info2 , R.drawable.info3 , R.drawable.info4 , R.drawable.info5};
     String txts[] = {"1번 설명","2번 설명","3번설명","4번설명","5번설명"};
@@ -21,6 +24,26 @@ public class infoActivity extends AppCompatActivity {
 
         img_info = findViewById(R.id.img_info);
         txt_info = findViewById(R.id.txt_info);
+        btn_info_back = findViewById(R.id.btn_info_back);
 
+        img_info.setImageResource(imgs[imgno]);
+        txt_info.setText(txts[imgno]);
+
+        img_info.setOnClickListener(this);
+        btn_info_back.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btn_info_back){
+
+            finish();
+        }else if(view.getId() ==R.id.img_info){
+
+            imgno =++imgno % 5;
+            img_info.setImageResource(imgs[imgno]);
+            txt_info.setText(txts[imgno]);
+        }
     }
 }
